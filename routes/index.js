11d2e795c195a8cbd7d5eb5218ae13d694a2bd91
todelusion <todelusion@gmail.com>
2024-11-28 11:25:31 +0800

    Update required params

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const crypto = require('crypto');
-require('dotenv').config();
+const crypto = require("crypto");
+require("dotenv").config();
 
 // 綠界提供的 SDK
-const ecpay_payment = require('ecpay_aio_nodejs');
+const ecpay_payment = require("ecpay_aio_nodejs");
 
 const { MERCHANTID, HASHKEY, HASHIV, HOST } = process.env;
 
 // SDK 提供的範例，初始化
 // https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
 const options = {
-  OperationMode: 'Test', //Test or Production
+  OperationMode: "Test", //Test or Production
   MercProfile: {
     MerchantID: MERCHANTID,
     HashKey: HASHKEY,
@@ -29,28 +29,31 @@ const options = {
 };
 let TradeNo;
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // SDK 提供的範例，參數設定
   // https://github.com/ECPay/ECPayAIO_Node.js/blob/master/ECPAY_Payment_node_js/conf/config-example.js
-  const MerchantTradeDate = new Date().toLocaleString('zh-TW', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
+  const MerchantTradeDate = new Date().toLocaleString("zh-TW", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
     hour12: false,
-    timeZone: 'UTC',
+    timeZone: "UTC",
   });
-  TradeNo = 'test' + new Date().getTime();
+  TradeNo = "test" + new Date().getTime();
   let base_param = {
     MerchantTradeNo: TradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
     MerchantTradeDate,
-    TotalAmount: '100',
-    TradeDesc: '測試交易描述',
-    ItemName: '測試商品等',
+    TotalAmount: "100",
+    TradeDesc: "測試交易描述",
+    ItemName: "測試商品等",
     ReturnURL: `${HOST}/return`,
     ClientBackURL: `${HOST}/clientReturn`,
+    PaymentType: "aio",
+    ChoosePayment: "ALL",
+    EncryptType: "1",
   };
   const create = new ecpay_payment(options);
 
@@ -58,15 +61,15 @@ router.get('/', (req, res) => {
   const html = create.payment_client.aio_check_out_all(base_param);
   console.log(html);
 
-  res.render('index', {
-    title: 'Express',
+  res.render("index", {
+    title: "Express",
     html,
   });
 });
 
 // 後端接收綠界回傳的資料
-router.post('/return', async (req, res) => {
-  console.log('req.body:', req.body);
+router.post("/return", async (req, res) => {
+  console.log("req.body:", req.body);
 
   const { CheckMacValue } = req.body;
   const data = { ...req.body };
@@ -76,20 +79,20 @@ router.post('/return', async (req, res) => {
   const checkValue = create.payment_client.helper.gen_chk_mac_value(data);
 
   console.log(
-    '確認交易正確性：',
+    "確認交易正確性：",
     CheckMacValue === checkValue,
     CheckMacValue,
-    checkValue,
+    checkValue
   );
 
   // 交易成功後，需要回傳 1|OK 給綠界
-  res.send('1|OK');
+  res.send("1|OK");
 });
 
 // 用戶交易完成後的轉址
-router.get('/clientReturn', (req, res) => {
-  console.log('clientReturn:', req.body, req.query);
-  res.render('return', { query: req.query });
+router.get("/clientReturn", (req, res) => {
+  console.log("clientReturn:", req.body, req.query);
+  res.render("return", { query: req.query });
 });
 
 module.exports = router;
